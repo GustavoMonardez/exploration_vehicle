00a63e81e65f5c6d8ff9efbe1a0367d654d03364
Gustavo Monardez <gustavomonardez@hotmail.com>
2021-02-26 18:18:47 -0800

    initial file creation

---
+++ exploration_vehicle.ino
@@ -0,0 +1,28 @@
+/*
+* Exploration Vehicle
+* Transmitter Code
+* Author: Gustavo Monardez
+*
+* D13   - SCLK                          D12   - MISO
+* A0    - J1_X                          D11   - MOSI
+* A1    - J1_Y                          D10   - CE
+* A2    - J2_Y                          D9    - CNS
+* A3    - J2_Y                          D8    -
+* A4    - SDA      (LCD and MPU-6050)   D7    -
+* A5    - SCL      (LCD and MPU-6050)   D6    - J2_SW
+* A6                                    D5    - J1_SW
+* A7                                    D4    - ROT ENC SW
+*                                       D3    - ROT ENC DT
+*                                       D2    - ROT ENC CLK
+*/
+
+
+
+void setup() {
+	
+}
+
+
+void loop() {
+	
+}
