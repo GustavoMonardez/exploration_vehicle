d0a7e5c1ad3e6df1ce990c736829ef94383f4d64
Gustavo Monardez <gustavomonardez@hotmail.com>
2021-03-03 15:10:26 -0800

    feat: add configuration prototype functions

--- Configurations.h
+++ Configurations.h
@@ -1,1 +1,18 @@
 #pragma once
+
+#include <RF24.h>
+#include <nRF24L01.h>
+
+void config_radio(RF24& radio);
+
+void config_joystick(const uint8_t vrx_pin, const uint8_t vrx_mode,
+	const uint8_t vry_pin, const uint8_t vry_mode,
+	const uint8_t sw_pin, const uint8_t sw_mode);
+
+void config_rot_encoder(const uint8_t clk_pin, const uint8_t clk_mode,
+	const uint8_t dt_pin, const uint8_t dt_mode,
+	const uint8_t sw_pin, const uint8_t sw_mode);
+
+void config_display(LiquidCrystal_I2C& lcd);
+
+void config_mpu_6050(const uint8_t mpu_addr);
