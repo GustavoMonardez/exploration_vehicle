7714d1161586847acdfc3ff1e4e72e71f93845d8
Gustavo Monardez <gustavomonardez@hotmail.com>
2021-03-04 15:35:33 -0800

    feat: add mpu6050 struct and process mpu6050 func prototype

--- Mpu6050.h
+++ Mpu6050.h
@@ -1,4 +1,15 @@
 #pragma once
 
+struct Mpu6050{
+    uint8_t left;
+    uint8_t right;
+    uint8_t down;
+    uint8_t up;
+
+    uint8_t device_addr;
+    uint8_t pwr_mgmt_reg_addr;
+    uint8_t start_data_addr;
+};
+
 void init_mpu_6050(const uint8_t mpu_addr, const uint8_t pwr_mgmt_reg);
-void calibrate_mpu_6050(const uint8_t mpu_addr, const uint8_t start_addr);
+void calibrate_mpu_6050(const uint8_t mpu_addr, const uint8_t data_addr);
--- ProcessDataOut.h
+++ ProcessDataOut.h
@@ -1,7 +1,9 @@
 #pragma once
 
 #include "Joystick.h"
+#include "Mpu6050.h"
 
 void process_joystick(Joystick& j);
 void process_joystick_alt(Joystick& j);
+void process_mpu_6050(Mpu6050& mpu);
 void send_data();
